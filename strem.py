ebacc3430451b04a07b6cef7d4dbbd3f7b2da3e2
Sharmila2502 <sharmibecse@gmail.com>
2025-08-20 11:48:19 +0530

    Add files via upload

---
+++ strem.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import pickle
+import streamlit as st
+import joblib
+encoder_path = "Desktop/encod_data.pkl" 
+model = joblib.load('Desktop/knn_joblib.pkl')
+
+with open(encoder_path, 'rb') as f:
+    encoder = pickle.load(f)
+
+
+st.title("Machine Learning Prediction App")
+st.header("Enter the details for prediction:")
+
+passenger_count = st.number_input('Passenger Count', min_value=1, max_value=6, step=1)
+distance_km=st.number_input('Trip Distance (km)', min_value=0.0, step=0.1)
+Duration=st.number_input('Duration', min_value=1, max_value=50, step=1)
+RatecodeID= st.selectbox('RateCode', ['RatecodeID_1','RatecodeID_2'])
+payment_type=st.selectbox('payment_type', ['payment_type_1','payment_type_2'])
+AM_PM=st.selectbox('AM_PM', ['AM','PM'])
+weekday = st.selectbox('Day of the Week', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+input_data = pd.DataFrame({
+    'passenger_count': [passenger_count],
+    'distance_km': [distance_km],
+    'Duration': [Duration],
+    'RatecodeID': [RatecodeID],
+    'payment_type': [payment_type],
+    'AM_PM': [AM_PM],
+    'weekday': [weekday],
+})
+categorical_cols=['RatecodeID','payment_type','AM_PM','weekday']
+continuous_cols=['passenger_count','distance_km','Duration']
+encoded_user_array = encoder.transform(input_data[categorical_cols])
+encoded_user_df = pd.DataFrame(encoded_user_array,columns=['RatecodeID_1','RatecodeID_2','payment_type_1','payment_type_2','AM','PM','weekday_1','weekday_3'])
+final_user_df = pd.concat([input_data[continuous_cols], encoded_user_df], axis=1)
+
+st.write("Input Data:", input_data)
+if st.button('Predict Trip Price'):
+    prediction = model.predict(final_user_df)
+    st.write(f"The predicted amount is {prediction}.")
+
+
+
